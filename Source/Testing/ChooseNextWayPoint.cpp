94a9f1e0617b841713527e71c346fc9f90c68ee1
Victor Garcia <victor.cortes96@gmail.com>
2019-10-20 18:51:37 +0200

    Adding the same behaviour with c++

---
+++ Source/Testing/ChooseNextWayPoint.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWayPoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" // Remove
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    /** Get Patrol Points
+    */
+    auto AIController = OwnerComp.GetAIOwner();
+    auto ControlledPawn = AIController->GetPawn();
+    auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+    auto PatrolPoints = PatrollingGuard->PatrolPoints;
+
+    /** Set next waypoint
+    */
+    auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+    auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    BlackBoardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
+
+    // TODO protect pointers
+
+    /** Cycle Index
+    */
+    auto NextIndex = (Index + 1) % PatrolPoints.Num();
+    BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+    UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+    
+    return EBTNodeResult::Succeeded;
+}
