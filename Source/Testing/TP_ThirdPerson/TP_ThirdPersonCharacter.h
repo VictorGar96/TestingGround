a3c91ef01bf86ea725d233b32db02207b0528eba
Victor Garcia <victor.cortes96@gmail.com>
2019-10-24 13:25:28 +0200

    Attaching mesh with Skeleton Sockets

--- Source/Testing/TP_ThirdPerson/TP_ThirdPersonCharacter.h
+++ Source/Testing/TP_ThirdPerson/TP_ThirdPersonCharacter.h
@@ -18,6 +18,7 @@ class ATP_ThirdPersonCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
 public:
 	ATP_ThirdPersonCharacter();
 
